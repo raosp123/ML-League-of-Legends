c4062ccc75d599630f0b638a7b30837a4d6209df
Pascal <raosp@tcd.ie>
2022-11-20 16:28:06 +0000

    did some looking into the returned json from api calls, and begun analyzing which json keys would be useful for our model analysis

--- getData.py
+++ getData.py
@@ -1,13 +1,32 @@
 import requests
+import json
+from keys import API_KEY
 
-api_key = 'RGAPI-ae3f51f8-1636-4c4a-b00d-33d4e93a5444'
-puuid = "S01KLVfg1APQYb5xtEoU7QcyKmlpxluPNatYIQE4wmWsallXAw0WBldrlXYMYwrug5PypPYmGtUBjA" # lubis
+puuid = "kmDsoNSAJDQ38U07YYem3xaUmPeFht0EouZ1h46WhvRYd8lJ4cVntEaSlrixvSTBl0SPnQLaM6pcpw" # mine
+matchid = "EUW1_6158939054"
+
+def getPlayerID(playerName):
+    response = requests.get(f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{playerName}?api_key={API_KEY}')
+    print(response.json())
+
+
+def getMatchIDs(playerID):
+
+    response = requests.get(f'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{playerID}/ids?api_key={API_KEY}')
+   
+    with open("./matchIDs.json", "w") as fp:
+        json.dump(response.json(), fp, indent=4)
+
+
+def getMatchData(matchID):
+
+    response =  requests.get(f'https://europe.api.riotgames.com/lol/match/v5/matches/{matchID}?api_key={API_KEY}')
+    with open("./matchData.json", "w") as fp:
+            json.dump(response.json(), fp, indent=4)
 
 
 
+#getMatchIDs(puuid)
+getMatchData(matchid)
 
-
-
-
-
-response = requests.get('https://euw1.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids')
+#getPlayerID('giantyoghurt')
